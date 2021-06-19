b7f63f00bc506d4201c211e18b1a8ef531ef03cb
Ivan_Babushkin <babushkinnavi.ib@gmail.com>
2021-06-20 02:52:11 +0300

    Validation Tests

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -5,7 +5,7 @@ import model.User;
 
 public class UserService {
     private static final String EMPTY_STRING = "";
-    private static final String REGEX_FOR_EMAIL = "^[^@\\s]+@[^@\\s\\.]+\\.[^@\\.\\s]+$";
+    private static final String REGEX_FOR_EMAIL = "^.+((@mail.ru)|(@gmail.com))$";
     private static final String REGEX_FOR_DATE = "^((2000|2400|2800|(19|2[0-9](0[48]|[2468][048]|[13579][26])))-02-29)$"
             + "|^(((19|2[0-9])[0-9]{2})-02-(0[1-9]|1[0-9]|2[0-8]))$"
             + "|^(((19|2[0-9])[0-9]{2})-(0[13578]|10|12)-(0[1-9]|[12][0-9]|3[01]))$"
---
+++ src/test/java/services/UserServiceTest.java
@@ -0,0 +1,96 @@
+package services;
+
+import exception.FormatDataException;
+import model.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.time.LocalDate;
+
+public class UserServiceTest {
+    private final UserService userService = new UserService();
+    private User user;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUp() {
+        user = new User(1L);
+        user.setEmail("test@mail.ru");
+        user.setFirstName("Name");
+        user.setLastName("LastName");
+        user.setBirthDate(LocalDate.of(2000, 1, 1));
+    }
+
+    @Test
+    public void testUserFieldsValidationCorrectUserSuccess() throws FormatDataException {
+        userService.userFieldsValidation(user);
+    }
+
+    @Test
+    public void testUserFieldsValidationFailsWithIncorrectEmail() throws FormatDataException {
+        thrown.expect(FormatDataException.class);
+        user.setEmail("incorrect@mail.rururu");
+        thrown.expectMessage("Email is incorrect: " + user.getEmail());
+        userService.userFieldsValidation(user);
+    }
+
+    @Test
+    public void testUserFieldsValidationFailsWithEmptyEmail() throws FormatDataException {
+        thrown.expect(FormatDataException.class);
+        user.setEmail("");
+        thrown.expectMessage("Email is incorrect: " + user.getEmail());
+        userService.userFieldsValidation(user);
+    }
+
+    @Test
+    public void testUserFieldsValidationFailsWithIncorrectFirstName() throws FormatDataException {
+        thrown.expect(FormatDataException.class);
+        user.setFirstName("!@#$%^");
+        thrown.expectMessage("First Name is incorrect: " + user.getFirstName());
+        userService.userFieldsValidation(user);
+    }
+
+    @Test
+    public void testUserFieldsValidationFailsWithEmptyFirstName() throws FormatDataException {
+        thrown.expect(FormatDataException.class);
+        user.setFirstName("");
+        thrown.expectMessage("Incorrect input data for user. All fields are required! Try again!");
+        userService.userFieldsValidation(user);
+    }
+
+    @Test
+    public void testUserFieldsValidationFailsWithIncorrectLastName() throws FormatDataException {
+        thrown.expect(FormatDataException.class);
+        user.setLastName("123");
+        thrown.expectMessage("Last Name is incorrect: " + user.getLastName());
+        userService.userFieldsValidation(user);
+    }
+
+    @Test
+    public void testUserFieldsValidationFailsWithEmptyLastName() throws FormatDataException {
+        thrown.expect(FormatDataException.class);
+        user.setLastName("");
+        thrown.expectMessage("Incorrect input data for user. All fields are required! Try again!");
+        userService.userFieldsValidation(user);
+    }
+
+    @Test
+    public void testDateFormatValidatorFailsWithIncorrectDate() {
+        UserService userService = new UserService();
+        user.setBirthDate(LocalDate.of(20000, 1, 1));
+        boolean isDateCorrect = userService.dateFormatValidator(String.valueOf(user.getBirthDate()));
+        Assert.assertFalse(isDateCorrect);
+    }
+
+    @Test
+    public void testDateFormatValidatorSuccess() {
+        UserService userService = new UserService();
+        boolean isDateCorrect = userService.dateFormatValidator(String.valueOf(user.getBirthDate()));
+        Assert.assertTrue(isDateCorrect);
+    }
+}
